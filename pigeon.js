2ee87b23c614a243b1208b4cc31273849921ec11
keanewong <keanewong@gmail.com>
2016-09-07 01:47:43 -0400

    added username support

--- pigeon.js
+++ pigeon.js
@@ -9,8 +9,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(msg, userName){
+    io.emit('chat message', msg, userName);
   });
 });
 
