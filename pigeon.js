34528737a4483d15c2df7a79771dac8bbca7e66f
keanewong <keanewong@gmail.com>
2016-10-18 00:01:25 -0400

    yay! images feature online!

--- pigeon.js
+++ pigeon.js
@@ -15,7 +15,6 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/assets'));
 
-
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg, {
